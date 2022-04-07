46380204a33df5738a591f118bee9dcd583fa88a
tafM12 <102439333+tafM12@users.noreply.github.com>
2022-04-07 06:25:11 +0100

    Update FilesInOut.java

--- FilesInOut.java
+++ FilesInOut.java
@@ -1,34 +1,53 @@
-import java.io.*;
-import java.awt.*;
-import java.awt.event.*;
 import java.util.*;
-import javax.swing.*;
-import java.lang.Number;
 
-/**
- * 
- * CSCU9T4 Java strings and files exercise.
- *
- */
-public class FilesInOut {
+// method simply checks if a string is of numeric value so we can correctly split name and date
+public class isNumeric {
+	public static boolean isNumeric(String str)
+	{ 
+		  try {  
+		    Double.parseDouble(str);  
+		    return true;
+		  } catch(NumberFormatException e){  
+		    return false;  
+		  }  
+	}
+	
+	public static String getName(String[] arr)
+	{
+		List<String>  nameList = new ArrayList();
+		for(int i = 0; i < arr.length ; i++)
+		{
+			if (isNumeric(arr[i]) == false)
+			{
+				nameList.add(arr[i]);
+			}
+		}
+		String name = nameList.toString();
+		return name;
+		
+	}
+	public static String getDate(String[] arr)
+	{
+		List<String>  dateList = new ArrayList();
+		for(int i = 0; i < arr.length ; i++)
+		{
+			if (isNumeric(arr[i]) == true)
+			{
+				dateList.add(arr[i]);
+			}
+			
+		}
+		String date = dateList.toString();
+		return date;
+		
+	}
+	
+	public static String[] lineSplit(String arr)
+	{
+		 String[] items = arr.split("\\s+");
+		 return items;
+		 
+		
+	}
+}
 
-    public static void main(String[] args) {
-        // Replace this with statements to set the file name (input) and file name (output).
-        // Initially it will be easier to hardcode suitable file names.
-
-        // Set up a new Scanner to read the input file.
-        // Processing line by line would be sensible here.
-        // Initially, echo the text to System.out to check you are reading correctly.
-        // Then add code to modify the text to the output format.
-
-        // Set up a new PrintWriter to write the output file.
-        // Add suitable code into the above processing (because you need to do this line by line also.
-        // That is, read a line, write a line, loop.
-
-        // Finally, add code to read the filenames as arguments from the command line.
-
-        System.out.println("You need to add your own code to do anything");
-
-    } // main
-
-} // FilesInOut
